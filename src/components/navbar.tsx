be2043ee0088d6c667ad32e3ca2df7f0fa1bb1ae
jruizsilva <ruizsilvajonathan@gmail.com>
2024-02-13 14:49:27 -0300

    responsive navbar styles done

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,12 @@
 'use client'
+import { Popover } from '@headlessui/react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { FaUserGroup } from 'react-icons/fa6'
+import { MdAccountCircle, MdLogout, MdMenu } from 'react-icons/md'
 import Button from './button'
 import MenuUser from './menuuser'
+import { IoMdSettings } from 'react-icons/io'
 
 interface NavbarProps {}
 
@@ -11,7 +15,7 @@ const Navbar = (props: NavbarProps) => {
     <div className={`shadow`}>
       <div className='flex items-center h-[68px] mx-auto px-6 py-2'>
         {/* Logo */}
-        <div className='flex justify-between items-center border-e-0 lg:border-e-[1px] border-[#e0e0e0] basis-[374px] pe-6 gap-4'>
+        <div className='flex justify-between items-center border-e-0 lg:border-e-[1px] border-[#e0e0e0] basis-full lg:basis-[374px] pe-6 gap-4'>
           <Link href='/'>
             <Image
               src={'/logo.svg'}
@@ -30,6 +34,42 @@ const Navbar = (props: NavbarProps) => {
           <ButtonSearch className='ms-auto' />
           <MenuUser />
         </div>
+        {/* Add a responsive menu button for small screens */}
+        <Popover className={'relative'}>
+          <Popover.Button>
+            <MdMenu fontSize={32} className='lg:hidden' />
+          </Popover.Button>
+          <Popover.Panel className='absolute right-[-24px] top-[58px] '>
+            <div className='flex flex-col w-screen max-w-96 rounded-xl border shadow-md p-4 '>
+              <div className='border-b flex flex-col mb-4'>
+                <h1 className='text-lg font-medium text-nowrap mb-2'>
+                  Devchallenges Board
+                </h1>
+                <ButtonSearch className='w-full' />
+              </div>
+              <div className='border-b flex flex-col'>
+                <button className='h-[39px] text-xs mb-2 rounded-lg bg-[#f2f2f2] font-medium hover:bg-[#e2e2e2] p-3 flex items-center gap-2'>
+                  <MdAccountCircle fontSize={'20px'} />
+                  <span>Dashboard</span>
+                </button>
+                <button className='h-[39px] text-xs mb-2 rounded-lg font-medium hover:bg-[#e2e2e2] p-3 flex items-center gap-2'>
+                  <FaUserGroup fontSize={'20px'} />
+                  <span>All boards</span>
+                </button>
+                <button className='h-[39px] text-xs mb-2 rounded-lg font-medium hover:bg-[#e2e2e2] p-3 flex items-center gap-2'>
+                  <IoMdSettings fontSize={'20px'} />
+                  <span>Setting</span>
+                </button>
+              </div>
+              <div className='pt-2'>
+                <button className='h-[39px] text-xs mb-2 rounded-lg font-medium hover:bg-[#ffdbdb] p-3 flex items-center gap-2 w-full'>
+                  <MdLogout fontSize={'20px'} fill='#EB5757' />
+                  <span className='text-[#EB5757]'>Logout</span>
+                </button>
+              </div>
+            </div>
+          </Popover.Panel>
+        </Popover>
       </div>
     </div>
   )
