546e2629a1a95f18622fa7bb6036c3ed5acd86fa
jruizsilva <ruizsilvajonathan@gmail.com>
2024-02-15 20:50:26 -0300

    attachments styles done

--- src/app/_ui/boardcard.tsx
+++ src/app/_ui/boardcard.tsx
@@ -2,12 +2,19 @@
 import Avatar from '@/components/avatar'
 import Button from '@/components/button'
 import CustomIcon from '@/components/navbar/customicon'
-import { notoSans } from '@/fonts/fonts'
+import { notoSans, poppins } from '@/fonts/fonts'
 import { Dialog, Transition } from '@headlessui/react'
 import Image from 'next/image'
 import { Fragment, useState } from 'react'
-import { MdAdd, MdAttachFile, MdComment } from 'react-icons/md'
+import {
+  MdAdd,
+  MdAttachFile,
+  MdComment,
+  MdDescription,
+  MdEdit
+} from 'react-icons/md'
 import BoardTag from './boardtag'
+import LabelWithIcon from './labelwithicon'
 
 interface Props {
   title: string
@@ -120,7 +127,7 @@ export default function BoardCard({
           </Transition.Child>
 
           <div className='fixed inset-0 overflow-y-auto'>
-            <div className='flex min-h-full items-center justify-center p-4 text-center'>
+            <div className='flex min-h-full items-center justify-center p-4'>
               <Transition.Child
                 as={Fragment}
                 enter='ease-out duration-300'
@@ -130,29 +137,126 @@ export default function BoardCard({
                 leaveFrom='opacity-100 scale-100'
                 leaveTo='opacity-0 scale-95'
               >
-                <Dialog.Panel className='w-[661px] max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
-                  <Dialog.Title
-                    as='h3'
-                    className='text-lg font-medium leading-6 text-gray-900'
+                <Dialog.Panel className='w-[661px] max-w-full transform overflow-hidden rounded-2xl bg-white p-6 shadow-xl transition-all'>
+                  <Image
+                    src='/logo.svg'
+                    alt='card cover image'
+                    width={616}
+                    height={130}
+                  />
+                  <h6
+                    className={`text-base font-normal text-black mt-6 mb-1 ${notoSans.className}`}
                   >
-                    Payment successful
-                  </Dialog.Title>
-                  <div className='mt-2'>
-                    <p className='text-sm text-gray-500'>
-                      Your payment has been successfully submitted.
-                      We’ve sent you an email with all of the details
-                      of your order.
+                    ✋🏿 Move anything that is actually started here
+                  </h6>
+                  <p
+                    className={`font-semibold text-[#bdbdbd] text-[10px] mb-5 ${poppins.className}`}
+                  >
+                    in list{' '}
+                    <span
+                      className={`text-[#333] font-semibold ${poppins.className}`}
+                    >
+                      In Progress
+                    </span>
+                  </p>
+                  <div className='flex items-center gap-4'>
+                    <LabelWithIcon
+                      icon={
+                        <CustomIcon
+                          icon={MdDescription}
+                          fill='#bdbdbd'
+                          fontSize={12}
+                        />
+                      }
+                    >
+                      Description
+                    </LabelWithIcon>
+                    <Button
+                      icon={
+                        <CustomIcon
+                          icon={MdEdit}
+                          fill='#828282'
+                          fontSize={12}
+                        />
+                      }
+                      className='w-[62px] h-6 border rounded-lg border-[#bdbdbd] flex items-center justify-center gap-2'
+                    >
+                      <span
+                        className={`font-medium text-[10px] text-[#828282] ${poppins.className}`}
+                      >
+                        Edit
+                      </span>
+                    </Button>
+                  </div>
+                  <div className='my-3'>
+                    <p
+                      className={`font-normal text-sm text-black ${poppins.className}`}
+                    >
+                      Ideas are created and share here through a card.
+                      Here you can describe what you&apos;d like to
+                      accomplish. For example you can follow three
+                      simple questions to create the card related to
+                      your idea: * Why ? (Why do you wish to do it ?)
+                      * What ? (What it is it, what are the goals, who
+                      is concerned) * How ? (How do you think you can
+                      do it ? What are the required steps ?) After
+                      creation, you can move your card to the todo
+                      list.
                     </p>
                   </div>
-
-                  <div className='mt-4'>
-                    <button
-                      type='button'
-                      className='inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2'
-                      onClick={closeModal}
-                    >
-                      Got it, thanks!
-                    </button>
+                  <LabelWithIcon
+                    icon={
+                      <CustomIcon
+                        icon={MdDescription}
+                        fill='#bdbdbd'
+                        fontSize={12}
+                      />
+                    }
+                    className='mb-3'
+                  >
+                    Attachments
+                  </LabelWithIcon>
+                  <div className='mb-6 flex gap-3'>
+                    <div className='h-[53px] w-[80px] rounded-lg flex items-center'>
+                      <Image
+                        src='/logo.svg'
+                        alt='preview file uploaded'
+                        width={80}
+                        height={53}
+                      />
+                    </div>
+                    <div className='flex flex-col'>
+                      <p className='text-[#bdbdbd] text-[8px]'>
+                        Added July 5, 2020
+                      </p>
+                      <p className='text-black text-[10px] mb-1'>
+                        Reasoning by Ranganath Krishnamani
+                      </p>
+                      <Button className='border rounded-lg border-[#bdbdbd] text-[#828282] w-[62px] h-[24px] text-[10px] font-medium'>
+                        Download
+                      </Button>
+                    </div>
+                  </div>
+                  <div className='mb-6 flex gap-3'>
+                    <div className='h-[53px] w-[80px] rounded-lg flex items-center'>
+                      <Image
+                        src='/logo.svg'
+                        alt='preview file uploaded'
+                        width={80}
+                        height={53}
+                      />
+                    </div>
+                    <div className='flex flex-col'>
+                      <p className='text-[#bdbdbd] text-[8px]'>
+                        Added July 5, 2020
+                      </p>
+                      <p className='text-black text-[10px] mb-1'>
+                        Reasoning by Ranganath Krishnamani
+                      </p>
+                      <Button className='border rounded-lg border-[#bdbdbd] text-[#828282] w-[62px] h-[24px] text-[10px] font-medium'>
+                        Download
+                      </Button>
+                    </div>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
